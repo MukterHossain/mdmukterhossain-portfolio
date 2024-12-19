6963611667277f44a7688b618fdd7bdf40c578dc
MukterHossain <mukter3075@gmail.com>
2024-12-19 23:12:40 +0600

    Navbar and some add

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,10 @@
+import Navbar from "../../Share/Navbar";
 
 
 const Home = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <h1>Welcome to my Home Page</h1>
         </div>
     );
